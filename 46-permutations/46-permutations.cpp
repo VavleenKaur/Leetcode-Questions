6bf1bc67499abfd02dc3f15e9ea16af4a91c11f7
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-01-22 23:46:53 +0530

    Time: 7 ms (27.39%), Space: 7.6 MB (91.86%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void findPer(vector<int>&nums,vector<vector<int>>&ans,vector<int>&ds,vector<int>&freq)
+    {
+        if(ds.size()==nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!freq[i])
+            {
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                findPer(nums,ans,ds,freq);
+                ds.pop_back();
+                freq[i]=0;
+            }
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        vector <int> freq(nums.size(),0);
+        findPer(nums,ans,ds,freq);
+        return ans;
+    }
+};
