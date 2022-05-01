abec0997fc19ad142d3219f32e2db66962992568
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-05-02 00:07:34 +0530

    Time: 7 ms (72.99%), Space: 9.2 MB (9.79%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -13,8 +13,21 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-       return f(0,0,triangle,n,dp);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int j=0;j<n;j++) {dp[n-1][j]=triangle[n-1][j];}
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+               
+                int down=0,diag=0;
+                down+=triangle[i][j]+dp[i+1][j];
+                diag+=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diag);
+                
+            }
+        }
+        return dp[0][0];
         
     }
 };
