80d4f754837a467742e80ffd3e3c8a362333a992
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-05-06 02:33:18 +0530

    Time: 17 ms (68.41%), Space: 11.9 MB (47.35%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -30,7 +30,20 @@ public:
         // sum=s1+s2 s1-s2=target sum=target+2s2
         if((sum-target)<0 || (sum-target)%2) return 0;
         int tar=(sum-target)/2;
-        vector<vector<int>>dp(n,vector<int>(tar+1,-1));
-        return f(n-1,tar,nums,dp);
+        vector<vector<int>>dp(n,vector<int>(tar+1,0));
+          if(nums[0] == 0)dp[0][0]=2;
+        else dp[0][0]=1;
+        if(nums[0]!=0 && nums[0]<=tar)dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=tar;j++)
+            {
+                int notake=dp[i-1][j];
+                int take=0;
+                if(nums[i]<=j) take=dp[i-1][j-nums[i]];
+                dp[i][j]=take+notake;
+            }
+        }
+        return dp[n-1][tar];
     }
 };
