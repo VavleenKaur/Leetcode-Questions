99afb2a2a109d01d3c98f4f75478b3617f3f2b99
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-05-06 12:43:18 +0530

    Time: 49 ms (33.59%), Space: 18.3 MB (41.05%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -16,6 +16,21 @@ public:
     int change(int amount, vector<int>& coins) {
        int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
+        for(int j=0;j<=amount;j++)
+        {
+            if(j%coins[0]==0) dp[0][j]=1;
+            else dp[0][j]=0;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                int notake=dp[i-1][j];
+                int take=0;
+                if(coins[i]<=j) take=dp[i][j-coins[i]];
+                dp[i][j]=notake+take;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
