c0550c4b7c5fd6c796f894ad364b357a90f4aba1
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-05-08 17:14:21 +0530

    Time: 11 ms (79.65%), Space: 6.4 MB (95.33%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,29 +3,28 @@ public:
     int minDistance(string str1, string str2) {
          int n=str1.size();
     int m=str2.size();
-    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-    for(int i=0;i<=n;i++)
-	{
-		dp[i][0]=i;
-	}
+    vector<int>prev(m+1,0); 
+    vector<int>cur(m+1,0);
+    
  for(int j=0;j<=m;j++)
 	{
-		dp[0][j]=j;
+		prev[j]=j;
 	}
  for(int i=1;i<=n;i++)
- {
+ {  cur[0]=i;
 	 for(int j=1;j<=m;j++)
 	 {
 		 if(str1[i-1]==str2[j-1])
 		 {
-			 dp[i][j]=dp[i-1][j-1];
+			 cur[j]=prev[j-1];
 		 }
 		 else
 		 {
-			 dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+			 cur[j]=1+min(prev[j-1],min(prev[j],cur[j-1]));
 		 }
 	 }
+  prev=cur;
  }
- return dp[n][m];
+ return prev[m];
     }
 };
