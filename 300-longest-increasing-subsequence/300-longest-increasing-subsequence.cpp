15ae37f662aeb9cc829a41c263e7b53c87606af9
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-05-09 18:18:44 +0530

    Time: 622 ms (10.74%), Space: 10.7 MB (18.69%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -12,20 +12,20 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+       vector<int>ahead(n+1,0),cur(n+1,0);
         for(int i=n-1;i>=0;i--)
         {
             for(int j=i-1;j>=-1;j--)
             {
-                int len=dp[i+1][j+1];
+                int len=ahead[j+1];
         if(j==-1 || nums[i]>nums[j])
         {
-            len=max(len,1+dp[i+1][i+1]);
+            len=max(len,1+ahead[i+1]);
         }
-                dp[i][j+1]=len;
+               cur[j+1]=len;
             }
-                
+             ahead=cur;   
         }
-        return dp[0][0];
+        return ahead[0];
     }
 };
