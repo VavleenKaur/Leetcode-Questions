3988ab0561f6b9b0829d9519b43c5e5d78644b26
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-07-07 21:48:14 +0530

    Time: 15 ms (28.32%), Space: 13.6 MB (80.15%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -6,7 +6,6 @@ public:
         {
             return n?cost[0]:0;
         }
-        vector<int>dp(n,-1);
         int prev2=cost[0];
         int prev=cost[1];
         for(int i=2;i<n;i++)
@@ -14,7 +13,6 @@ public:
             int f_step=prev2+cost[i];
             int s_step=prev+cost[i];
             int curr=min(f_step,s_step);
-            dp[i]=curr;
             prev2=prev;
             prev=curr;
         }
@@ -23,7 +21,7 @@ public:
     // int memo(vector<int>& cost,int ind)
     // {
     //    if(ind==0 || ind==1) return cost[ind];
-    //     int oneStep=0,twoStep=0;
+    //     int oneStep=0,twoStep=;
     //     oneStep=memo(cost,ind-1)+abs(cost[ind]);
     //     twoStep=memo(cost,ind-2)+abs(cost[ind]);
     //     return min(oneStep,twoStep);
