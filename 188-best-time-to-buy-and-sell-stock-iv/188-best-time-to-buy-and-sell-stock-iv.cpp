bd59147b3bbdd91702a55351cce87a0115a1e441
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-07-16 02:27:10 +0530

    Time: 11 ms (75.37%), Space: 10.9 MB (75.32%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int memo(int ind,int canBuy,int k,vector<vector<vector<int>>>& dp,vector<int>&prices,int n)
-    {   if(k==0) return 0;
-        if(ind==n) return 0;
-        if(dp[ind][canBuy][k]!=-1) return dp[ind][canBuy][k];
-        if(canBuy)
-        {
-           return dp[ind][canBuy][k]=max(-prices[ind]+memo(ind+1,0,k,dp,prices,n),0+memo(ind+1,1,k,dp,prices,n));
-        }
-        else
-        {
-           return dp[ind][canBuy][k]=max(prices[ind]+memo(ind+1,1,k-1,dp,prices,n),0+memo(ind+1,0,k,dp,prices,n));
-        }
-    }
+    // int memo(int ind,int canBuy,int k,vector<vector<vector<int>>>& dp,vector<int>&prices,int n)
+    // {   if(k==0) return 0;
+    //     if(ind==n) return 0;
+    //     if(dp[ind][canBuy][k]!=-1) return dp[ind][canBuy][k];
+    //     if(canBuy)
+    //     {
+    //        return dp[ind][canBuy][k]=max(-prices[ind]+memo(ind+1,0,k,dp,prices,n),0+memo(ind+1,1,k,dp,prices,n));
+    //     }
+    //     else
+    //     {
+    //        return dp[ind][canBuy][k]=max(prices[ind]+memo(ind+1,1,k-1,dp,prices,n),0+memo(ind+1,0,k,dp,prices,n));
+    //     }
+    // }
     int maxProfit(int k, vector<int>& prices) {
        int n=prices.size();
-       vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return memo(0,1,k,dp,prices,n);
-        
+       vector<vector<int>>ahead(2,vector<int>(k+1,0));
+       vector<vector<int>>curr(2,vector<int>(k+1,0));
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            
+                for(int trans=1;trans<=k;trans++)
+                {
+           curr[1][trans]=max(-prices[ind]+ahead[0][trans],0+ahead[1][trans]);
+       
+            curr[0][trans]=max(prices[ind]+ahead[1][trans-1],0+ahead[0][trans]);
+    
+                }
+            ahead=curr;
+            
+        }
+        return ahead[1][k];
     }
+    
 };
