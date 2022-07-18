b5cc0a7a3df7b7fb22461ae38ddc1bad50ecaa9f
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-07-18 21:30:44 +0530

    Time: 59 ms (81.28%), Space: 9 MB (17.52%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -2,33 +2,38 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n=nums.size();
+        if(nums.size()<=1) return nums;
         sort(nums.begin(),nums.end());
         vector<int>dp(n,1);
-        vector<int>hash(n);
-        int maxi=1,lastindex=0;
-        for(int i=0;i<n;i++)
-        {   hash[i]=i;
-            for(int j=0;j<i;j++)
-            {
-               if(nums[i]%nums[j]==0 && 1+dp[j]>dp[i])
-               {
-                   dp[i]=1+dp[j];
-                   hash[i]=j;
-               }
+        vector<int>hash(n,1);
+        int maxi=0,last_index=0;
+        for(int i=1;i<n;i++)
+        {  
+            for(int prev=0;prev<i;prev++)
+            {   
+                if(nums[i]% nums[prev]==0)
+                {
+                    if(dp[prev]+1>dp[i])
+                    {
+                        dp[i]=dp[prev]+1;
+                        hash[i]=prev;
+                    }
+                }
             }
             if(dp[i]>maxi)
             {
-                maxi=dp[i];
-                lastindex=i;
+               maxi=dp[i];
+               last_index=i;
             }
         }
-        vector<int>temp;
-        temp.push_back(nums[lastindex]);
-        while(hash[lastindex]!=lastindex)
-        {    lastindex=hash[lastindex];
-            temp.push_back(nums[lastindex]);
+        
+        vector<int>res(maxi);
+        for(int i=maxi-1;i>=0;i--)
+        {
+            res[i]=nums[last_index];
+            last_index=hash[last_index];
         }
-        reverse(temp.begin(),temp.end());
-        return temp;
+        
+        return res;
     }
 };
