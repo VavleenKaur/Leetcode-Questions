472957c82e8b950ce2806e6dd1213807134fee78
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-07-25 18:27:36 +0530

    Time: 157 ms (5.04%), Space: 287.9 MB (5.01%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool memo(int inds,int indt,string s, string t,vector<vector<int>>&dp)
     {
-        if(inds<0) return true;
-        if(inds>=0 && indt<0) return false;
+        if(inds==0) return true;
+        if(inds>=1 && indt==0) return false;
         if(dp[inds][indt]!=-1) return dp[inds][indt];
-        if(s[inds]==t[indt]) return dp[inds][indt]=memo(inds-1,indt-1,s,t,dp);
+        if(s[inds-1]==t[indt-1]) return dp[inds][indt]=memo(inds-1,indt-1,s,t,dp);
         else
         {
             return dp[inds][indt]=memo(inds,indt-1,s,t,dp);
@@ -15,6 +15,6 @@ public:
         int lens=s.size();
         int lent=t.size();
         vector<vector<int>>dp(lens+1,vector<int>(lent+1,-1));
-        return memo(lens-1,lent-1,s,t,dp);
+        return memo(lens,lent,s,t,dp);
     }
 };
