0f99a03b2b8a595edb4a7edfa477c6a961c7a23c
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-07-25 12:11:31 +0530

    Time: 7 ms (81.79%), Space: 13.7 MB (48.06%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
+    int memo(vector<int>&cost,int ind,vector<int>&dp)
+    {
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int mincost=1e9;
+        mincost=min(cost[ind]+memo(cost,ind-1,dp),cost[ind]+memo(cost,ind-2,dp));
+        return dp[ind]=mincost;
+    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        if(n<=1)
-        {
-            return n?cost[0]:0;
-        }
-        int prev2=cost[0];
-        int prev=cost[1];
-        for(int i=2;i<n;i++)
-        {
-            int f_step=prev2+cost[i];
-            int s_step=prev+cost[i];
-            int curr=min(f_step,s_step);
-            prev2=prev;
-            prev=curr;
-        }
-        return min(prev,prev2);
+        vector<int>dp(n+1,-1);
+        return min(memo(cost,n-1,dp),memo(cost,n-2,dp));
     }
-    // int memo(vector<int>& cost,int ind)
-    // {
-    //    if(ind==0 || ind==1) return cost[ind];
-    //     int oneStep=0,twoStep=;
-    //     oneStep=memo(cost,ind-1)+abs(cost[ind]);
-    //     twoStep=memo(cost,ind-2)+abs(cost[ind]);
-    //     return min(oneStep,twoStep);
-    // }
 };
