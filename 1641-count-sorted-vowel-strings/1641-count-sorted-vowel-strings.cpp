9a3ccd0c68cde70b818cc8bb5d88a96dcfe3b281
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-07-25 23:15:16 +0530

    Time: 5 ms (27.41%), Space: 8 MB (11.04%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
-    int countVowelStrings(int n) {
-           int a=1, e=1, i=1, o=1, u=1;
-        
-        while(--n){
-            o += u;
-            i += o;
-            e += i;
-            a += e;
+    
+    int memo(int n,char last_char,vector<vector<int>>&dp)
+    {
+        if(n==0) return 1;
+        if(dp[n][int(last_char)]!=-1) return dp[n][int(last_char)];
+        if(last_char=='\0')
+        {
+            return dp[n][int(last_char)]=(memo(n-1,'a',dp)+memo(n-1,'e',dp)+memo(n-1,'i',dp)+memo(n-1,'o',dp)+memo(n-1,'u',dp));
         }
+        else if(last_char=='a')
+        {
+            return dp[n][int(last_char)]=memo(n-1,'a',dp);
+        }
+        else if(last_char=='e')
+        {
+            return dp[n][int(last_char)]=memo(n-1,'a',dp)+memo(n-1,'e',dp);
+        }
+        else if(last_char=='i')
+        {
+            return dp[n][int(last_char)]=memo(n-1,'a',dp)+memo(n-1,'e',dp)+memo(n-1,'i',dp);
+        }
+        else if(last_char=='o')
+        {
+            return dp[n][int(last_char)]=(memo(n-1,'a',dp)+memo(n-1,'e',dp)+memo(n-1,'i',dp)+memo(n-1,'o',dp));
+        }
+        else if(last_char=='u')
+        {
+            return dp[n][int(last_char)]=(memo(n-1,'a',dp)+memo(n-1,'e',dp)+memo(n-1,'i',dp)+memo(n-1,'o',dp)+memo(n-1,'u',dp));
+        }
+        else return dp[n][int(last_char)]=0;
         
-        return a+e+i+o+u;
+    }
+    int countVowelStrings(int n) {
+        if(n==1) return 5;
+        vector<vector<int>>dp(n+1,vector<int>(257,-1));
+       return memo(n,'\0',dp);
     }
 };
