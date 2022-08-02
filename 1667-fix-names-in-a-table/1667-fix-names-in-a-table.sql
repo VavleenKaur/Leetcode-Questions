f804c1df88cbcea7b102a64f4d098c645f974c6b
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-08-02 13:43:12 +0530

    Time: 537 ms (93.94%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,3 +1,3 @@
-/* Write your PL/SQL query statement below */
-SELECT user_id AS user_id ,INITCAP(name) AS name from Users
+# Write your MySQL query statement below
+SELECT user_id AS user_id ,CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name,LENGTH(name)-1)))AS name from Users
 order by user_id;
