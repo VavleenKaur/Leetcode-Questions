80fea992e08f2d5e87de772a337f38d9fc02f4cb
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-08-22 11:02:42 +0530

    Time: 4 ms (90.07%), Space: 7.3 MB (66.07%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     
-    
-    void sol(int col,vector<vector<string>>&ans,vector<string>&board,vector<int>&leftRow,vector<int>&lowerDiag,vector<int>&upperDiag,int n)
+    void rec(int col, int n,vector<string>&board,vector<vector<string>>&ans,vector<int>&left,vector<int>&upleftdiag,vector<int>&downleftdiag)
     {
         if(col==n)
         {
@@ -11,33 +10,29 @@ public:
         }
         for(int row=0;row<n;row++)
         {
-            if(leftRow[row]==0 && lowerDiag[row+col]==0 && upperDiag[n-1+col-row]==0)
+            if(board[row][col]=='.' && left[row]==0 && upleftdiag[row+col]==0 && downleftdiag[n-1+col-row]==0)
             {
                 board[row][col]='Q';
-                leftRow[row]=1;
-                lowerDiag[row+col]=1;
-                upperDiag[n-1+col-row]=1;
-                sol(col+1,ans,board,leftRow,lowerDiag,upperDiag,n);
+                left[row]=1; 
+                upleftdiag[row+col]=1;
+                downleftdiag[n-1+col-row]=1; rec(col+1,n,board,ans,left,upleftdiag,downleftdiag);
                 board[row][col]='.';
-                leftRow[row]=0;
-                lowerDiag[row+col]=0;
-                upperDiag[n-1+col-row]=0;
+                left[row]=0; 
+                upleftdiag[row+col]=0;
+                downleftdiag[n-1+col-row]=0;
             }
         }
     }
+    
+    
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<string> board(n);
+       
         string s(n,'.');
-        for(int i=0;i<n;i++)
-        {
-           board[i]=s;
-        }
-        vector<int>leftRow(n,0);
-        vector<int>lowerDiag(2*n-1,0);
-        //sum of row and col index 0-2n-1
-        vector<int>upperDiag(2*n-1,0);
-        sol(0,ans,board,leftRow,lowerDiag,upperDiag,n);
+        vector<string>board(n,s);
+        vector<vector<string>>ans;
+        vector<int>left(n,0);
+        vector<int>upleftdiag(2*n-1,0),downleftdiag(2*n-1,0);
+        rec(0,n,board,ans,left,upleftdiag,downleftdiag);
         return ans;
     }
 };
