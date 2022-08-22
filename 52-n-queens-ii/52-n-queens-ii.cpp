d1dd5b13ebcd9c83117f5116d2a3f8f6f204fbee
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-08-22 11:12:20 +0530

    Time: 6 ms (70.00%), Space: 6.1 MB (83.58%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void rec(int col, int n,vector<string>&board,int &ans,vector<int>&left,vector<int>&upleftdiag,vector<int>&downleftdiag)
+    int rec(int col, int n,vector<string>&board,vector<int>&left,vector<int>&upleftdiag,vector<int>&downleftdiag)
     {
         if(col==n)
         {
-            ans++;
-            return;
+            return 1;
         }
+        int res=0;
         for(int row=0;row<n;row++)
         {
             if(board[row][col]=='.' && left[row]==0 && upleftdiag[row+col]==0 && downleftdiag[n-1+col-row]==0)
@@ -14,21 +14,21 @@ public:
                 board[row][col]='Q';
                 left[row]=1; 
                 upleftdiag[row+col]=1;
-                downleftdiag[n-1+col-row]=1; rec(col+1,n,board,ans,left,upleftdiag,downleftdiag);
+                downleftdiag[n-1+col-row]=1; res+=rec(col+1,n,board,left,upleftdiag,downleftdiag);
                 board[row][col]='.';
                 left[row]=0; 
                 upleftdiag[row+col]=0;
                 downleftdiag[n-1+col-row]=0;
             }
         }
+        return res;
     }
     int totalNQueens(int n) {
         string s(n,'.');
         vector<string>board(n,s);
-        int ans=0;
         vector<int>left(n,0);
         vector<int>upleftdiag(2*n-1,0),downleftdiag(2*n-1,0);
-        rec(0,n,board,ans,left,upleftdiag,downleftdiag);
-        return ans;
+       return  rec(0,n,board,left,upleftdiag,downleftdiag);
+      
     }
 };
