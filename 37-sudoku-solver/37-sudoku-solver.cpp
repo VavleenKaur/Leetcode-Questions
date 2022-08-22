11dfe8b00c750a3c786de73b3cb16a9f3b2f3366
Vavleen Kaur <70514344+VavleenKaur@users.noreply.github.com>
2022-08-22 11:43:41 +0530

    Time: 47 ms (49.70%), Space: 6.4 MB (79.83%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    
-    bool solve(vector<vector<char>>&board)
+    bool recur(vector<vector<char>>& board)
     {
-        for(int i=0;i<board.size();i++)
+        for(int i=0;i<9;i++)
         {
-            for(int j=0;j<board[0].size();j++)
+            for(int j=0;j<9;j++)
             {
                 if(board[i][j]=='.')
                 {
-                    for(char c='1';c<='9';c++)
+                    for(char k='1';k<='9';k++)
                     {
-                        if(isValid(c,board,i,j))
-                        {  board[i][j]=c;
-                            if(solve(board))
-                            {
-                                return true;
-                            }
-                            else
-                            {
-                                board[i][j]='.'; //backtrack
-                            }
+                        if(isValid(i,j,k,board))
+                        {
+                            board[i][j]=k;
+                            if(recur(board)) return true;
+                            else board[i][j]='.';
                         }
                     }
                     return false;
@@ -28,30 +22,20 @@ public:
             }
         }
         return true;
-        
     }
     
-    bool isValid(char c,vector<vector<char>>&board,int row,int col)
+    bool isValid(int row,int col,char k, vector<vector<char>>& board)
     {
         for(int i=0;i<9;i++)
         {
-            if(board[row][i]==c)
-            {
-                return false;
-            }
-            if(board[i][col]==c)
-            {
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
-            {
-                return false;
-            }
+            if(board[i][col]==k) return false;
+            if(board[row][i]==k) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==k) return false;
         }
         return true;
     }
     
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        recur(board);
     }
 };
